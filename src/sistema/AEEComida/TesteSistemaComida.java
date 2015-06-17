d2db154843c85835bf1ebbae26119b64ff6f712f
Marcos Inacio de Paula Lima <marcos.inacio.recife@hotmail.com>
2015-06-17 16:45:16 -0300

    alt 2

--- src/sistema/AEEComida/TesteSistemaComida.java
+++ src/sistema/AEEComida/TesteSistemaComida.java
@@ -5,6 +5,7 @@ import sistema.AEEComida_negocio.*;
 public class TesteSistemaComida {
 
 	public static void main(String[] args) {
+		System.out.println("TesteProject!");
 		Menu meuM = new Menu();
 		meuM.meuMenu();
 		
